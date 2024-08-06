3565f1c113e7719d7a19f9c798dc35f2b7516ea4
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-08-06 17:01:02 +0000

    Update stateful class with state properties

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 class StatefulGreeting  extends React.Component{
+
+    constructor() {
+        super();
+        this.state = {};
+    }
+
     render() {
         return <h1>Hello from StatefulGreeting</h1>
     }
